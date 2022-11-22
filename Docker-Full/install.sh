b35b3a3cbaa2a23970cca42dfd2122c39901f565
Jeff <jeffbena@gmail.com>
2022-11-22 13:58:20 +0100

    typo

--- Docker-Full/install.sh
+++ Docker-Full/install.sh
@@ -14,4 +14,4 @@ apt install docker-compose-plugin
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
-echo "done installing! You can run docker ps to see portainer running"`
+echo "done installing! You can run docker ps to see portainer running"
